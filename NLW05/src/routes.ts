7266181bd5eabe2e52a3da325b3b70ed8a065d76
Júlio Ferreira <juliof0_@hotmail.com>
2021-04-24 17:43:52 -0300

    Add files via upload

---
+++ NLW05/src/routes.ts
@@ -0,0 +1,31 @@
+import { request, response, Router} from "express";
+import { MessagesController } from "./controllers/MessagesController";
+import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
+
+
+const routes = Router();
+
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
+
+/* Tipos de parametros
+    Routes Params => Parametros de rotas
+    https://localhost:3333/settings/1
+    Query Params => Filtros e buscas
+    https://localhost:3333/settings/1?search=algumacoisa
+    Body params => {
+        json
+    }
+**
+*/
+routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+routes.post("/messages", messagesController.create);
+routes.get("/settings/:username", settingsController.findByUsername);
+routes.put("/settings/:username", settingsController.update);
+routes.get("/messages/:id", messagesController.showByUser);
+
+
+export { routes}
---
+++ NLW05/src/services/UsersService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
+import { UserRepository } from "../repositories/UsersRepository"
+
+
+class UsersService {
+
+    private usersRepository: Repository<User>
+    constructor(){
+        this.usersRepository = getCustomRepository(UserRepository);
+    }
+    async create(email: string){
+
+        
+        //Verificar se o usuário existe
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        //Se existir, retornar user
+
+        if(userExists){
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+
+        //Se não existir salvar no DB
+
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email });
+      
+        return user;
+      }
+}
+
+export {UsersService}
